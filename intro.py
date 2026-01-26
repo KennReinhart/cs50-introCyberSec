478723285ade73c2dc2e46f2f95044f1a5523b4e
KennReinhart <kenfmr@gmail.com>
2026-01-26 18:49:25 +0700

    #Securing Accounts

---
+++ intro.py
@@ -0,0 +1,30 @@
+from string import ascii_lowercase, digits, punctuation
+fusion = ascii_lowercase + digits + punctuation
+
+#Securing Accounts
+'''
+Gabungan antara ascii, digit atau angka dan tanda baca dalam pass 
+menjadikan pass tidak mudah ditebak. Komputer dapat menebak pass tersebut
+dengan berdasarkan kompleksitas yang rumit. Contoh dibawah ini :
+4 digits, miliseconds
+4 fusion, about 3 minutes
+
+social engineering, 
+phishing, gathering by faking or impersonating page or other forms
+single sign on (SSO),
+
+1:03:25
+use of password managers
+'''
+for i in fusion:
+    for j in fusion:
+        for k in fusion:
+            for l in fusion:
+                print(i, j, k, l)
+
+#Securing Data
+'''
+
+'''
+
+
